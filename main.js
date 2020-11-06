4ae9351578b2196f873d4be79e7f871d3f8b095c
John Lester <john.matthew.lester@gmail.com>
2020-11-05 20:15:05 -0600

    first update

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ const replaceAllButton = document.querySelector(".replace-all-button")
 // this array.
 const rowElements = document.querySelectorAll(".row")
 
+let currentRowArray = []
+
 // When you call the function belwo, it will get and return an INNER ARRAY
 // containing the cell elements for a given row.
 // Call this function from WITHIN your row elements loop. Then you will, in turn,
@@ -17,10 +19,54 @@ const rowElements = document.querySelectorAll(".row")
 // NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
+   
 }
 
 
 // YOUR CODE GOES HERE
+
+
+
+// make a function that checks each row
+replaceAllButton.addEventListener("click",function(){
+    let findValue= findInput.value;
+    let replaceValue = replaceInput.value;
+ 
+    console.log ("search started")
+    console.log (findInput.value)
+    console.log (replaceInput.value)
+for (let i = 0; i < rowElements.length; i++){;
+    console.log('i', i);
+    console.log(rowElements[i]);
+     let cell = getCellElements(rowElements[i]);
+     console.log (cell);
+    if (cell[i].includes(findValue)){;
+        console.log('Item found');
+        rowElements[i].replaceWith(replaceValue);
+    }else
+console.log('not found')
+    
+
+//check each item in the row.
+for (let j = 0; j < cell.length; j++){
+    // let cells = getCellElements()
+    console.log(cell[j].innerHTML)
+    console.log('j', j)
+    console.log('i', i)
+    if (findInput.value.includes(rowElements[j])){
+        
+        rowElements.replace(replaceInput.value)
+    }else
+console.log('not found')
+    
+
+}
+
+
+
+}
+})
+
 
 
 // One last thing: dedicate very careful attention to using variables and
